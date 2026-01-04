5ee29e8e6b4d346166adac55cb8e280642fd6932
ametsforal <aforalgarc1@educacion.navarra.es>
2026-01-04 20:57:02 +0100

    Clean up Program.cs by removing commented code
    Removed commented-out code and unnecessary lines.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,16 +8,8 @@ class Program
 {
     public static void Main(string[] args)
     {
-        /* Eskaerak l = new Eskaerak();
-        l.MenuGehitu("Pizza");
-        l.MenuGehitu("Pasta");
-        l.PizzaPertsonalizatua(new List<string> { "Tomate", "Gazta", "Olibak" });
-        l.EskaeraErakutsi();
-        Console.WriteLine();
-        l.EskaeraHustu();
-        l.EskaeraErakutsi(); */
-
         Menuak l = new Menuak();
         l.Run();
     }
 }
+
